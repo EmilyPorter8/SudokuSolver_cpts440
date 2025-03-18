095af031238540612e36be3f520c8948ed9995e9
Brenden <120139082+NednerbLlenrad@users.noreply.github.com>
2025-03-18 14:12:24 -0700

    Created miniSat output

---
+++ SAT_IO/miniSat_Output_Parse_Print.py
@@ -0,0 +1,30 @@
+def parse_miniSat_output(filename):
+    with open(filename, "r") as f:
+        lines = f.readlines()
+    
+    if lines[0].strip() != "SAT":
+        return None #No solution present
+    
+    assignments = map(int, lines[1].split())
+    grid = [[0]*9 for _ in range(9)]
+    
+    for var in assignments:
+        if var > 0:
+            x = var - 1
+            r = x // 81 + 1
+            c = (x % 81) // 9 + 1
+            v = (x % 9) + 1
+            grid[r - 1][c - 1] = v
+            
+    return grid
+
+def print_sudoku(grid):
+    for row in grid:
+        print( " ".join(map(str, row)))
+         
+#Test Case:
+sudoku_solution = parse_miniSat_output(r"SAT_IO\sudoku.out")
+if sudoku_solution:
+    print_sudoku(sudoku_solution)
+else:
+    print("No solution found.")
