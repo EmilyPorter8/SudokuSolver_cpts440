56b6bbebe2306d22d1279e530cd7c3d7b4e7373a
Brenden <120139082+NednerbLlenrad@users.noreply.github.com>
2025-03-18 14:48:32 -0700

    Changed Output script to generic size

--- SAT_IO/miniSat_Output_Parse_Print.py
+++ SAT_IO/miniSat_Output_Parse_Print.py
@@ -1,6 +1,6 @@
 #Inputs a CNF file containing the solution to the sudoku problem. Outputs it as a grid.
 #As of right now, is set to a 9x9 for base case
-def parse_miniSat_output(filename):
+def parse_miniSat_output(filename, n=100):
     with open(filename, "r") as f:
         lines = f.readlines()
     
@@ -8,14 +8,14 @@ def parse_miniSat_output(filename):
         return None #No solution present
     
     assignments = map(int, lines[1].split())
-    grid = [[0]*9 for _ in range(9)]
+    grid = [[0]*n for _ in range(n)]
     
     for var in assignments:
         if var > 0:
             x = var - 1
-            r = x // 81 + 1
-            c = (x % 81) // 9 + 1
-            v = (x % 9) + 1
+            r = x // (n*n) + 1
+            c = (x % (n*n)) // n + 1
+            v = (x % n) + 1
             grid[r - 1][c - 1] = v
             
     return grid
@@ -25,7 +25,7 @@ def print_sudoku(grid):
         print( " ".join(map(str, row)))
          
 #Test Case:
-sudoku_solution = parse_miniSat_output(r"SAT_IO\sudoku.out")
+sudoku_solution = parse_miniSat_output(r"SAT_IO\sudoku.out", n=100)
 if sudoku_solution:
     print_sudoku(sudoku_solution)
 else:
