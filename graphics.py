0a9442edf1767b8320ca945a83e1bf6945655a9c
EmilyPorter84 <emilyporter79@gmail.com>
2025-04-03 11:00:00 -0700

    select options for solver

--- graphics.py
+++ graphics.py
@@ -9,6 +9,9 @@ root.title("Sudoku Solver")
 input_frame = tk.Frame(root)
 input_frame.pack()
 
+selected_solver = tk.StringVar()
+selected_solver.set("Z3")
+solvers = ["Z3", "DFS", "Whatever else"]
 
 boardsize = tk.IntVar()
 slider = tk.Scale( input_frame, variable = boardsize,  
@@ -29,11 +32,25 @@ def generateboard():
     sheet = tksheet.Sheet(board_frame)
     sheet.grid()
     create_sudoku_sheet()
+    solve_board_bt['state'] = tk.NORMAL
     #sheet.pack(expand = True, fill = "both")
     #board = Sudoku_Generator.run(size)
 
 make_board = tk.Button(input_frame, text="make the board", command=generateboard)
 make_board.pack()
+
+def solveboard():
+    print(selected_solver)
+    if selected_solver.get() == "Z3":
+        print("Solving board for Z3...")
+    elif selected_solver.get() == "DFS":
+        print("Solving board for DFS...")
+    return
+
+select_solver_menu = tk.OptionMenu(input_frame, selected_solver, *solvers)
+select_solver_menu.pack()
+solve_board_bt = tk.Button(input_frame, text = "Solve the board", state = tk.DISABLED,command = solveboard)
+solve_board_bt.pack()
 
 def create_sudoku_sheet():
     global sheet
@@ -43,10 +60,6 @@ def create_sudoku_sheet():
         return
 
     actual_size = size * size
-
-    #replace with actual data
-    # test_data = [[f"{ri},{cj}" for cj in range(actual_size)] for ri in range(actual_size)]
-    # sheet.set_sheet_data(test_data)
     BackendBoard = Sudoku_Generator.run(size)
     test_data = [[
     " " if BackendBoard.valueReturn(cj, ri) == 0 else f"{BackendBoard.valueReturn(cj, ri)}"
